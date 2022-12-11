24e14c7c1e962b78b9c44fdc6513a792b89b38e3
Julian <jumpingafterrain@gmail.com>
2022-12-11 21:30:04 +0100

    fix 12 am

--- public/lib/msToTime.js
+++ public/lib/msToTime.js
@@ -2,50 +2,34 @@ export default function msToTime(duration, twelveHours) {
   let milliseconds = Math.floor((duration % 1000) / 100),
     seconds = Math.floor((duration / 1000) % 60),
     minutes = Math.floor((duration / (1000 * 60)) % 60),
-    hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
-    // days = Math.floor(duration / (1000 * 60 * 60 * 24))
+    hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+  // days = Math.floor(duration / (1000 * 60 * 60 * 24))
 
-  hours = hours < 10 ? '0' + hours : hours
-  minutes = minutes < 10 ? '0' + minutes : minutes
-  seconds = seconds < 10 ? '0' + seconds : seconds
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
 
-  if(twelveHours) {
-    let hoursGreaterThanTwelve = null
-    if(hours > 12) {
-      hoursGreaterThanTwelve = hours - 12
-      return(
-        hoursGreaterThanTwelve +
-        ':' +
-        minutes +
-        ' PM'
-      )
-    } else if(hours == 12) {
-      return(
-        hours +
-        ':' +
-        minutes +
-        ' PM' 
-      )
-    } else {
-      return(
-        hours +
-        ':' +
-        minutes +
-        ' AM' 
-      )
+  // handle 12 hour clock output
+  if (twelveHours) {
+    let hoursGreaterThanTwelve = null;
+    if (hours == "00") {
+      return "12" + ":" + minutes + " AM";
     }
+    if (hours > 12) {
+      hoursGreaterThanTwelve = hours - 12;
+      return hoursGreaterThanTwelve + ":" + minutes + " PM";
+    }
+    if (hours == 12) {
+      return hours + ":" + minutes + " PM";
+    }
+
+    return hours + ":" + minutes + " AM";
   }
 
   return (
     // 'Days: ' +
     // days +
     // '........' +
-    hours +
-    ':' +
-    minutes +
-    ':' +
-    seconds +
-    '.' +
-    milliseconds
-  )
+    hours + ":" + minutes + ":" + seconds + "." + milliseconds
+  );
 }
