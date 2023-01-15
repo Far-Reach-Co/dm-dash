5763c676b2d1f92a98d53139176236dda9aa260d
Julian <jumpingafterrain@gmail.com>
2023-01-16 00:46:17 +0100

    little refactoring of clocks

--- public/views/Clocks.js
+++ public/views/Clocks.js
@@ -25,47 +25,7 @@ export default class ClocksView {
     }
   };
 
-  newClock = async () => {
-    if (!state.clocks) return;
-
-    var projectId = state.currentProject;
-    try {
-      const res = await fetch(`${window.location.origin}/api/add_clock`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          title: "New Clock",
-          current_time_in_milliseconds: 0,
-          project_id: projectId,
-        }),
-      });
-      const data = await res.json()
-      if (res.status === 201) {
-        const clock = data;
-        const clockComponentDomElement = createElement("div", {
-          id: `clock-component-${clock.id}`,
-        });
-        // append
-        this.domComponent.appendChild(clockComponentDomElement);
-        // instantiate
-        const newClock = new Clock({
-          domComponent: clockComponentDomElement,
-          id: clock.id,
-          title: clock.title,
-          currentTimeInMilliseconds: clock.current_time_in_milliseconds,
-          parentRender: this.render,
-        });
-        state.clockComponents[`project-${state.currentProject}`].push(newClock);
-      } else throw new Error();
-    } catch (err) {
-      window.alert("Failed to create new clock...");
-      console.log(err);
-    }
-  };
-
-  render = async () => {
-    this.domComponent.innerHTML = "";
-
+  getClockElements = async () => {
     // ******** CLOCKS
     let clockData = [];
     const clockElements = [];
@@ -110,22 +70,64 @@ export default class ClocksView {
         state.clockComponents[`project-${state.currentProject}`].push(newClock);
       });
     }
+    return clockElements;
+  };
+
+  newClock = async () => {
+    if (!state.clocks) return;
+
+    var projectId = state.currentProject;
+    try {
+      const res = await fetch(`${window.location.origin}/api/add_clock`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          title: "New Clock",
+          current_time_in_milliseconds: 0,
+          project_id: projectId,
+        }),
+      });
+      const data = await res.json();
+      if (res.status === 201) {
+        const clock = data;
+        const clockComponentDomElement = createElement("div", {
+          id: `clock-component-${clock.id}`,
+        });
+        // append
+        this.domComponent.appendChild(clockComponentDomElement);
+        // instantiate
+        const newClock = new Clock({
+          domComponent: clockComponentDomElement,
+          id: clock.id,
+          title: clock.title,
+          currentTimeInMilliseconds: clock.current_time_in_milliseconds,
+          parentRender: this.render,
+        });
+        state.clockComponents[`project-${state.currentProject}`].push(newClock);
+      } else throw new Error();
+    } catch (err) {
+      window.alert("Failed to create new clock...");
+      console.log(err);
+    }
+  };
+
+  render = async () => {
+    this.domComponent.innerHTML = "";
+
     // append
     this.domComponent.append(
-      createElement(
-        "button",
-        { style: "align-self: flex-end;" },
-        "+ Clock",
-        {type: "click", event: this.newClock}
-      ),
-      createElement("h1", {style: "align-self: center;"}, "Clocks"),
+      createElement("button", { style: "align-self: flex-end;" }, "+ Clock", {
+        type: "click",
+        event: this.newClock,
+      }),
+      createElement("h1", { style: "align-self: center;" }, "Clocks"),
       createElement(
         "small",
         { style: "align-self: center;" },
         "* Clocks are auto saved every 60 seconds while running, or when stop is pressed"
       ),
       createElement("br"),
-      ...clockElements
+      ...(await this.getClockElements())
     );
   };
 }
