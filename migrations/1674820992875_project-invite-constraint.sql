19d571f87b6fff7b80b7e75fbdc62bceb22a4758
Julian <jumpingafterrain@gmail.com>
2023-01-27 13:04:51 +0100

    fix drop line

--- migrations/1674820992875_project-invite-constraint.sql
+++ migrations/1674820992875_project-invite-constraint.sql
@@ -3,4 +3,4 @@ ALTER TABLE public."ProjectInvite"
 ADD CONSTRAINT UC_Project UNIQUE (project_id);
 -- Down Migration
 ALTER TABLE public."ProjectInvite"
-DROP CONSTRAINT UC_Project UNIQUE (project_id);
+DROP CONSTRAINT UC_Project;
