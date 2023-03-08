f290491cab73ed55b150fc0ea327ff2d869bd9a4
Julian <jumpingafterrain@gmail.com>
2023-03-08 18:09:09 +0100

    finishing touches

--- public/views/Players.js
+++ public/views/Players.js
@@ -1,5 +1,7 @@
-import { getThings } from "../lib/apiUtils.js";
+import { deleteThing, getThings, postThing } from "../lib/apiUtils.js";
 import createElement from "../lib/createElement.js";
+import renderLoadingWithMessage from "../lib/loadingWithMessage.js";
+import playerCharacterSelect from "../lib/playerCharacterSelect.js";
 import state from "../lib/state.js";
 
 export default class PlayersView {
@@ -8,28 +10,162 @@ export default class PlayersView {
     this.domComponent.className = "standard-view";
     this.navigate = props.navigate;
 
+    this.connect = false;
+    this.connectLoading = false;
+
     this.render();
   }
 
+  toggleConnect = () => {
+    this.connect = !this.connect;
+    this.render();
+  };
+
+  toggleConnectLoading = () => {
+    this.connectLoading = !this.connectLoading;
+    this.render();
+  };
+
+  addConnection = async (e) => {
+    const formData = new FormData(e.target);
+    const formProps = Object.fromEntries(formData);
+    formProps.project_id = state.currentProject.id;
+    if (Object.values(formProps)[0] != 0) {
+      this.toggleConnectLoading();
+      await postThing(`/api/add_project_player`, formProps);
+      this.toggleConnectLoading();
+    }
+  };
+
+  renderCurrentConnections = async () => {
+    const projectPlayerIds = await getThings(
+      `/api/get_project_players_by_project/${state.currentProject.id}`
+    );
+    if (!projectPlayerIds.length)
+      return [createElement("small", {}, "None...")];
+
+    let myPlayerCharacters = await getThings(`/api/get_5e_characters_by_user`);
+    return await Promise.all(
+      myPlayerCharacters
+        .filter((pc) => {
+          for (var pp of projectPlayerIds) {
+            return pp.player_id === pc.id;
+          }
+        })
+        .map(async (player) => {
+          if (player) {
+            const projectPlayerId = projectPlayerIds.filter(
+              (pp) => player.id === pp.player_id
+            )[0].id;
+            const elem = createElement(
+              "div",
+              { style: "margin-left: 10px; display: flex;" },
+              [
+                player.name,
+                createElement(
+                  "div",
+                  {
+                    style:
+                      "color: var(--red1); margin-left: 10px; cursor: pointer;",
+                  },
+                  "ⓧ",
+                  {
+                    type: "click",
+                    event: () => {
+                      deleteThing(
+                        `/api/remove_project_player/${projectPlayerId}`
+                      );
+                      elem.remove();
+                    },
+                  }
+                ),
+              ]
+            );
+            return elem;
+          }
+        })
+    );
+  };
+
+  renderConnect = async () => {
+    if (this.connectLoading) {
+      return this.domComponent.append(
+        renderLoadingWithMessage(
+          "Please wait while we setup your connection..."
+        )
+      );
+    }
+
+    this.domComponent.append(
+      createElement("div", {}, [
+        createElement("h1", {}, `Connect Players`),
+        createElement("hr"),
+        createElement("h2", {}, "Current connections"),
+        ...(await this.renderCurrentConnections()),
+        createElement("hr"),
+        createElement("h2", {}, "Add connections"),
+        createElement(
+          "form",
+          {},
+          [
+            await playerCharacterSelect(),
+            createElement("br"),
+            createElement(
+              "button",
+              { class: "new-btn", type: "submit" },
+              "Add"
+            ),
+          ],
+          {
+            type: "submit",
+            event: async (e) => {
+              e.preventDefault();
+              await this.addConnection(e);
+            },
+          }
+        ),
+        createElement("hr"),
+        createElement("button", {}, "Done", {
+          type: "click",
+          event: () => {
+            this.toggleConnect();
+          },
+        }),
+      ])
+    );
+  };
+
   renderCharacterList = async () => {
+    // get project players list
+    const projectPlayers = await getThings(
+      `/api/get_project_players_by_project/${state.currentProject.id}`
+    );
+    // if not the creator of this project only view your own characters
     if (state.currentProject.wasJoined) {
       const sheetData = await getThings("/api/get_5e_characters_by_user");
       if (!sheetData.length) {
         return [createElement("div", {}, "*You have not linked a player yet.")];
       }
-      return sheetData.map(characterSheet => {
-        const elem = createElement("div");
-        new PlayerComponent({
-          domComponent: elem,
-          sheet: characterSheet,
-          navigate: this.navigate,
+      const sheetMap = sheetData
+        .filter((characterSheet) => {
+          for (var projectPlayer of projectPlayers) {
+            return projectPlayer.player_id === characterSheet.id;
+          }
+        })
+        .map((characterSheet) => {
+          const elem = createElement("div");
+          new PlayerComponent({
+            domComponent: elem,
+            sheet: characterSheet,
+            navigate: this.navigate,
+          });
+          return elem;
         });
-        return elem;
-      })
+      if (!sheetMap.length) {
+        return [createElement("div", {}, "*You have not linked a player yet.")];
+      } else return sheetMap;
     } else {
-      const projectPlayers = await getThings(
-        `/api/get_project_players_by_project/${state.currentProject.id}`
-      );
+      // show all connected characters
       if (!projectPlayers.length) {
         return [createElement("div", {}, "*No players have been linked yet.")];
       }
@@ -51,7 +187,21 @@ export default class PlayersView {
   };
 
   render = async () => {
-    this.domComponent.append(...(await this.renderCharacterList()));
+    this.domComponent.innerHTML = "";
+
+    if (this.connect) {
+      return this.renderConnect();
+    }
+
+    this.domComponent.append(
+      createElement("button", {}, "🔗 Player Character", {
+        type: "click",
+        event: this.toggleConnect,
+      }),
+      createElement("hr"),
+      createElement("br"),
+      ...(await this.renderCharacterList())
+    );
   };
 }
 
