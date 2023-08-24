840c336631ea06f0fc55e545b9712e351fd4680c
Julian <jumpingafterrain@gmail.com>
2023-08-24 13:56:32 +0200

    fix build

--- dist/api/controllers/botChar.js
+++ dist/api/controllers/botChar.js
@@ -171,7 +171,7 @@ function handleGetGeneralInfoResponse(charGeneralId, res) {
             const spellInfo = spellInfoData.rows[0];
             let content = "";
             content += `**${charGeneral.name}**`;
-            content += `\n**Race:** ${charGeneral.name}`;
+            content += `\n**Race:** ${charGeneral.race}`;
             content += `\n**Class:** ${charGeneral.class}`;
             if (charGeneral.subclass)
                 content += `\n**Sub-Class:** ${charGeneral.subclass}`;
