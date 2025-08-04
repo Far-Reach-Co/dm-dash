8265cf9a7c6be5d3019bf169ad3e301354e85351
Julian Ranieri <68655515+royalsaltmerchant@users.noreply.github.com>
2025-08-04 14:16:00 -0700

    rollback again

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -147,7 +147,7 @@ const upload = multer({ dest: "file_uploads/" });
 const csrf = require("csurf");
 
 //csrf use
-const csrfMiddleware = csrf({ cookie: true });
+const csrfMiddleware = csrf({ cookie: false });
 
 var router = Router();
 
